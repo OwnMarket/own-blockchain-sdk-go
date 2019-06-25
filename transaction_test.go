374a3df7187e449f6574458942f7388f6f2955c7
Marian Gheorghe <marian.gheorghe@chainium.io>
2019-06-25 11:34:29 +0200

    Add TransferChxAction test

--- transaction_test.go
+++ transaction_test.go
@@ -7,6 +7,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// TX
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 func TestCreateTxCorrectJson(t *testing.T) {
 	senderWallet := GenerateWallet()
 	expectedJson :=
@@ -20,6 +24,56 @@ func TestCreateTxCorrectJson(t *testing.T) {
 }`, senderWallet.Address)
 
 	tx := CreateTx(senderWallet.Address, 1, 0.01, 123)
+	actualJson := tx.ToJson(true)
+	assert.Equal(t, expectedJson, actualJson)
+}
+
+func TestCreateTxExpirationTimeZeroIfNotProvided(t *testing.T) {
+	senderWallet := GenerateWallet()
+	expectedJson :=
+		fmt.Sprintf(
+			`{
+    "senderAddress": "%s",
+    "nonce": 1,
+    "expirationTime": 0,
+    "actionFee": 0.01,
+    "actions": []
+}`, senderWallet.Address)
+
+	tx := CreateTx(senderWallet.Address, 1, 0.01, 0)
+	actualJson := tx.ToJson(true)
+	assert.Equal(t, expectedJson, actualJson)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Actions: Network Management
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func TestAddTransferChxAction(t *testing.T) {
+	senderWallet := GenerateWallet()
+	recipientWallet := GenerateWallet()
+	var amount float64 = 1000
+
+	expectedJson :=
+		fmt.Sprintf(
+			`{
+    "senderAddress": "%s",
+    "nonce": 1,
+    "expirationTime": 0,
+    "actionFee": 0.01,
+    "actions": [
+        {
+            "actionType": "TransferChx",
+            "actionData": {
+                "recipientAddress": "%s",
+                "amount": %3.0f
+            }
+        }
+    ]
+}`, senderWallet.Address, recipientWallet.Address, amount)
+
+	tx := CreateTx(senderWallet.Address, 1, 0.01, 0)
+	tx.AddTransferChxAction(recipientWallet.Address, amount)
 	actualJson := tx.ToJson(true)
 	assert.Equal(t, expectedJson, actualJson)
 }
--- transactions.go
+++ transactions.go
@@ -10,8 +10,8 @@ import (
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 type TxAction struct {
-	ActionType string          `json:"actionType"`
-	ActionData json.RawMessage `json:"actionData"`
+	ActionType string      `json:"actionType"`
+	ActionData interface{} `json:"actionData"`
 }
 
 type Tx struct {
@@ -26,6 +26,15 @@ type SignedTx struct {
 	Tx        string `json:"tx"`
 	Signature string `json:"signature"`
 }
+
+type TransferChxTxActionDto struct {
+	RecipientAddress string  `json:"recipientAddress"`
+	Amount           float64 `json:"amount"`
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Constructor
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 func CreateTx(senderAddress string, nonce int64, actionFee float64, expirationTime int64) *Tx {
 	tx := &Tx{
@@ -43,96 +52,99 @@ func CreateTx(senderAddress string, nonce int64, actionFee float64, expirationTi
 // Actions
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-func (tx Tx) addAction(actionType string, actionData json.RawMessage) {
+func (tx *Tx) addAction(actionType string, actionData interface{}) {
 	txAction := TxAction{ActionType: actionType, ActionData: actionData}
 	tx.Actions = append(tx.Actions, txAction)
 }
 
-func (tx Tx) AddTransferChxAction(recipientAddress string, amount float64) {
-	jsonString := fmt.Sprintf("{recipientAddress: %s, amount: %f}", recipientAddress, amount)
-	tx.addAction("TransferChx", json.RawMessage(jsonString))
+func (tx *Tx) AddTransferChxAction(recipientAddress string, amount float64) {
+	dto := TransferChxTxActionDto{
+		RecipientAddress: recipientAddress,
+		Amount:           amount,
+	}	
+	tx.addAction("TransferChx", dto)
 }
 
-func (tx Tx) AddDelegateStakeAction(validatorAddress string, amount float64) {
+func (tx *Tx) AddDelegateStakeAction(validatorAddress string, amount float64) {
 	jsonString := fmt.Sprintf("{validatorAddress: %s, amount: %f}", validatorAddress, amount)
 	tx.addAction("DelegateStake", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddConfigureValidatorAction(networkAddress string, sharedRewardPercent float64, isEnabled bool) {
+func (tx *Tx) AddConfigureValidatorAction(networkAddress string, sharedRewardPercent float64, isEnabled bool) {
 	jsonString := fmt.Sprintf("{networkAddress: %s, sharedRewardPercent: %f, isEnabled: %t}", networkAddress, sharedRewardPercent, isEnabled)
 	tx.addAction("ConfigureValidator", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddRemoveValidatorAction() {
+func (tx *Tx) AddRemoveValidatorAction() {
 	tx.addAction("DelegateStake", json.RawMessage("{}"))
 }
 
-func (tx Tx) AddTransferAssetAction(fromAccountHash string, toAccountHash string, assetHash string, amount float64) {
+func (tx *Tx) AddTransferAssetAction(fromAccountHash string, toAccountHash string, assetHash string, amount float64) {
 	jsonString := fmt.Sprintf("{fromAccountHash: %s, toAccountHash: %s, assetHash: %s, amount: %f}", fromAccountHash, toAccountHash, assetHash, amount)
 	tx.addAction("TransferAsset", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddCreateAssetEmissionAction(emissionAccountHash string, assetHash string, amount float64) {
+func (tx *Tx) AddCreateAssetEmissionAction(emissionAccountHash string, assetHash string, amount float64) {
 	jsonString := fmt.Sprintf("{emissionAccountHash: %s, assetHash: %s, amount: %f}", emissionAccountHash, assetHash, amount)
 	tx.addAction("CreateAssetEmission", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddCreateAssetAction() {
+func (tx *Tx) AddCreateAssetAction() {
 	tx.addAction("CreateAsset", json.RawMessage("{}"))
 	// TODO: return derive hash
 }
 
-func (tx Tx) AddSetAssetCodeAction(assetHash string, assetCode string) {
+func (tx *Tx) AddSetAssetCodeAction(assetHash string, assetCode string) {
 	jsonString := fmt.Sprintf("{assetHash: %s, assetCode: %s}", assetHash, assetCode)
 	tx.addAction("SetAssetCode", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddSetAssetControllerAction(assetHash string, controllerAddress string) {
+func (tx *Tx) AddSetAssetControllerAction(assetHash string, controllerAddress string) {
 	jsonString := fmt.Sprintf("{assetHash: %s, controllerAddress: %s}", assetHash, controllerAddress)
 	tx.addAction("SetAssetController", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddCreateAccountAction() {
+func (tx *Tx) AddCreateAccountAction() {
 	tx.addAction("CreateAccount", json.RawMessage("{}"))
 	// TODO: return derive hash
 }
 
-func (tx Tx) AddSetAccountControllerAction(accountHash string, controllerAddress string) {
+func (tx *Tx) AddSetAccountControllerAction(accountHash string, controllerAddress string) {
 	jsonString := fmt.Sprintf("{accountHash: %s, controllerAddress: %s}", accountHash, controllerAddress)
 	tx.addAction("SetAccountController", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddSubmitVoteAction(accountHash string, assetHash string, resolutionHash string, voteHash string) {
+func (tx *Tx) AddSubmitVoteAction(accountHash string, assetHash string, resolutionHash string, voteHash string) {
 	jsonString := fmt.Sprintf("{accountHash: %s, assetHash: %s, resolutionHash: %s, voteHash: %s}", accountHash, assetHash, resolutionHash, voteHash)
 	tx.addAction("SubmitVote", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddSubmitVoteWeightAction(accountHash string, assetHash string, resolutionHash string, voteWeight float64) {
+func (tx *Tx) AddSubmitVoteWeightAction(accountHash string, assetHash string, resolutionHash string, voteWeight float64) {
 	jsonString := fmt.Sprintf("{accountHash: %s, assetHash: %s, resolutionHash: %s, voteWeight: %f}", accountHash, assetHash, resolutionHash, voteWeight)
 	tx.addAction("SubmitVoteWeight", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddSetAccountEligibilityAction(accountHash string, assetHash string, isPrimaryEligible bool, isSecondaryEligible bool) {
+func (tx *Tx) AddSetAccountEligibilityAction(accountHash string, assetHash string, isPrimaryEligible bool, isSecondaryEligible bool) {
 	jsonString := fmt.Sprintf("{accountHash: %s, assetHash: %s, isPrimaryEligible: %t, isSecondaryEligible: %t}", accountHash, assetHash, isPrimaryEligible, isSecondaryEligible)
 	tx.addAction("SetAccountEligibility", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddSetAssetEligibilityAction(assetHash string, isEligibilityRequired bool) {
+func (tx *Tx) AddSetAssetEligibilityAction(assetHash string, isEligibilityRequired bool) {
 	jsonString := fmt.Sprintf("{assetHash: %s, isEligibilityRequired: %t}", assetHash, isEligibilityRequired)
 	tx.addAction("SetAssetEligibility", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddChangeKycControllerAddressAction(accountHash string, assetHash string, kycControllerAddress string) {
+func (tx *Tx) AddChangeKycControllerAddressAction(accountHash string, assetHash string, kycControllerAddress string) {
 	jsonString := fmt.Sprintf("{accountHash: %s, assetHash: %s, kycControllerAddress: %s}", accountHash, assetHash, kycControllerAddress)
 	tx.addAction("ChangeKycControllerAddress", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddAddKycProviderAction(assetHash string, providerAddress string) {
+func (tx *Tx) AddAddKycProviderAction(assetHash string, providerAddress string) {
 	jsonString := fmt.Sprintf("{assetHash: %s, providerAddress: %s}", assetHash, providerAddress)
 	tx.addAction("AddKycProvider", json.RawMessage(jsonString))
 }
 
-func (tx Tx) AddRemoveKycProviderAction(assetHash string, providerAddress string) {
+func (tx *Tx) AddRemoveKycProviderAction(assetHash string, providerAddress string) {
 	jsonString := fmt.Sprintf("{assetHash: %s, providerAddress: %s}", assetHash, providerAddress)
 	tx.addAction("RemoveKycProvider", json.RawMessage(jsonString))
 }
@@ -141,7 +153,7 @@ func (tx Tx) AddRemoveKycProviderAction(assetHash string, providerAddress string
 // Signing
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-func (tx Tx) ToJson(indentation bool) string {
+func (tx *Tx) ToJson(indentation bool) string {
 	var b []byte
 	var err error
 	if indentation {
@@ -158,7 +170,7 @@ func (tx Tx) ToJson(indentation bool) string {
 	return string(b)
 }
 
-func (tx Tx) sign(networkCode []byte, privateKey string) *SignedTx {
+func (tx *Tx) sign(networkCode []byte, privateKey string) *SignedTx {
 	json := tx.ToJson(false)
 	signature := SignMessage(networkCode, privateKey, []byte(json))
 	signedTx := &SignedTx{
